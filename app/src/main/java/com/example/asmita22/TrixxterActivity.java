586fd27c197b1183b42f8fc27f3c9cc89382bc39
AashishA11 <76701146+AashishA11@users.noreply.github.com>
2022-01-20 19:16:59 +0530

    done

--- app/src/main/java/com/example/asmita22/TrixxterActivity.java
+++ app/src/main/java/com/example/asmita22/TrixxterActivity.java
@@ -1,14 +1,29 @@
 package com.example.asmita22;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.asmita22.Adaptor.fragmentAdaptor;
+import com.google.android.material.tabs.TabLayout;
+
 public class TrixxterActivity extends AppCompatActivity {
 
+    TabLayout tabLayout;
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trixxter);
+
+        getSupportActionBar().hide();
+
+
+        viewPager=findViewById(R.id.ViewPager);
+        viewPager.setAdapter(new fragmentAdaptor(getSupportFragmentManager()));
+
+        tabLayout=findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
