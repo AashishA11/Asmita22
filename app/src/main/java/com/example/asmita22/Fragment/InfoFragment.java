ddeb4a9b20828c201e0091f9242647e625127604
Tabish Malik <76650437+Dark-Hope-tech@users.noreply.github.com>
2022-01-24 23:05:24 +0530

    added insta and fb link

--- app/src/main/java/com/example/asmita22/Fragment/InfoFragment.java
+++ app/src/main/java/com/example/asmita22/Fragment/InfoFragment.java
@@ -1,5 +1,7 @@
 package com.example.asmita22.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -9,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,7 +31,8 @@ import java.lang.reflect.Field;
 
 public class InfoFragment extends Fragment {
     FirebaseFirestore firestore;
-    public  TextView info_txt,insta_link,fb_link;
+    public  TextView info_txt;
+    public ImageView insta_link,fb_link;
     View view=null;
     public InfoFragment() {
         // Required empty public constructor
@@ -49,16 +53,30 @@ public class InfoFragment extends Fragment {
         view=inflater.inflate(R.layout.fragment_info, container, false);
         firestore=FirebaseFirestore.getInstance();
         info_txt=(TextView) view.findViewById(R.id.info_TV);
-        insta_link=(TextView) view.findViewById(R.id.tv_insta);
-        fb_link=(TextView) view.findViewById(R.id.tv_fb);
+        insta_link=(ImageView) view.findViewById(R.id.iv_insta);
+        fb_link=(ImageView) view.findViewById(R.id.iv_facebook);
         DocumentReference documentReference=firestore.collection("events").document("BGMI");
         documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
                 if(documentSnapshot.exists()){
                     info_txt.setText(documentSnapshot.getString("Info"));
-                    insta_link.setText(documentSnapshot.getString("InstaLink"));
-                    fb_link.setText(documentSnapshot.getString("FacebookLink"));
+                    String url_insta=documentSnapshot.getString("InstaLink");
+                    String url_fb=documentSnapshot.getString("FacebookLink");
+                    insta_link.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url_insta));
+                            startActivity(browserIntent);
+                        }
+                    });
+                    fb_link.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url_fb));
+                            startActivity(browserIntent);
+                        }
+                    });
                 }
             }
         });
