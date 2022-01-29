bcaf3a8b7d10c0456ed75f18f672a1d592a34ca7
AashishA11 <76701146+AashishA11@users.noreply.github.com>
2022-01-29 17:50:46 +0530

    logo icon added

--- app/src/main/java/com/example/asmita22/SplashScreen.java
+++ app/src/main/java/com/example/asmita22/SplashScreen.java
@@ -1,8 +1,8 @@
 package com.example.asmita22;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
@@ -11,10 +11,7 @@ import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
-
 public class SplashScreen extends AppCompatActivity {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
@@ -23,10 +20,8 @@ public class SplashScreen extends AppCompatActivity {
             public void run() {
                 Intent i=new Intent(SplashScreen.this,MainActivity.class);
                 //Intent is used to switch from one activity to another.
-
                 startActivity(i);
                 //invoke the SecondActivity.
-
                 finish();
                 //the current activity will get finished.
             }
